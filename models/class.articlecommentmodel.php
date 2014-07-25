c93018570769f3a97014f23c01e575c4e0686242
Shadowdare <shadowdare@users.noreply.github.com>
2014-07-24 18:28:19 -0700

    Implement ArticleCommentModel->Delete() method.

--- models/class.articlecommentmodel.php
+++ models/class.articlecommentmodel.php
@@ -76,4 +76,42 @@ class ArticleCommentModel extends Gdn_Model {
 
         return $Comment;
     }
+
+    /**
+     * Delete a comment.
+     *
+     * This is a hard delete that completely removes it from the database.
+     * Events: DeleteComment.
+     *
+     * @param int $CommentID Unique ID of the comment to be deleted.
+     * @param array $Options Additional options for the delete.
+     *
+     * @returns true on successful delete; false if comment ID doesn't exist.
+     */
+    public function Delete($CommentID, $Options = array()) {
+        $this->EventArguments['CommentID'] = &$CommentID;
+
+        $Comment = $this->GetByID($CommentID);
+        if (!$Comment)
+            return false;
+
+        $this->FireEvent('DeleteComment');
+
+        // Log the deletion.
+        $Log = GetValue('Log', $Options, 'Delete');
+        LogModel::Insert($Log, 'ArticleComment', $Comment, GetValue('LogOptions', $Options, array()));
+
+        // Delete the comment.
+        $this->SQL->Delete('ArticleComment', array('CommentID' => $CommentID));
+
+        $Article = $this->SQL->GetWhere('Article', array('ArticleId' => $Comment->ArticleID))->FirstRow();
+
+        // TODO: Update the comment count
+
+        // TODO: Update the user's comment count
+
+        // TODO: Update the category count and last comment info.
+
+        return true;
+    }
 }
