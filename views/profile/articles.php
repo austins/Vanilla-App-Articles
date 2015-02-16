8a8fef113086e3ed448a03caeae2b6f0433be809
Shadowdare <shadowdare@users.noreply.github.com>
2015-02-16 14:15:23 -0800

    Target profile articles view as well for float fixes and refactor ul to section tag

--- views/profile/articles.php
+++ views/profile/articles.php
@@ -5,9 +5,9 @@ echo '<h2 class="H">' . T('Articles') . '</h2>';
 if (!$this->Data('Articles') || count($this->Data('Articles')) == 0) {
     echo Wrap(T("This user has not published any articles yet."), 'div', array('Class' => 'Empty'));
 } else {
-    echo '<ul class="DataList Articles">';
+    echo '<section class="Articles">';
     include($this->FetchViewLocation('index', 'articles', 'Articles'));
-    echo '</ul>';
+    echo '</section>';
 
     echo $this->Pager->ToString('more');
 }
