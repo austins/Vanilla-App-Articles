82ba50fac760c7285e87365b3291a7c28b266e30
Shadowdare <shadowdare@users.noreply.github.com>
2015-06-30 19:19:28 -0700

    Move "New Article" filter link to its own button in the compose dashboard

--- views/modules/composefilter.php
+++ views/modules/composefilter.php
@@ -4,6 +4,15 @@ $Controller = Gdn::Controller();
 $ControllerName = strtolower($Controller->ControllerName);
 $RequestMethod = strtolower($Controller->RequestMethod);
 ?>
+<div class="BoxButtons BoxNewArticle">
+    <?php
+    echo Anchor(T('New Article'), '/compose/article',
+        'Button Action Big Primary BigButton NewArticle');
+
+    Gdn::Controller()->FireEvent('AfterNewArticleButton');
+    ?>
+</div>
+
 <div class="BoxFilter BoxComposeFilter">
     <ul class="FilterMenu">
         <li <?php if ($RequestMethod == 'index') echo 'class="Active"'; ?>>
@@ -13,7 +22,5 @@ $RequestMethod = strtolower($Controller->RequestMethod);
         <li <?php if ($RequestMethod == 'posts') echo 'class="Active"'; ?>>
             <?php echo Anchor(T('Posts'), '/compose/posts'); ?>
         </li>
-
-        <li><?php echo Anchor(T('New Article'), '/compose/article'); ?></li>
     </ul>
 </div>
